16fb7b6c75bff9e789477a0e179d4fb0f51db0a4
Salvatore Uras <yogavidya@gmail.com>
2021-04-25 04:31:00 +0200

    Add project files.

---
+++ Controllers/UploadLogFileController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TrimanAssessment.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadLogFileController : ControllerBase
+    {
+        private readonly ILogger<UploadLogFileController> _logger;
+
+        [HttpPost]
+        public JsonResult ReceiveUpload([FromForm] IFormCollection filesData)
+        {
+            var responseBodyMap = new Dictionary<string, string>();
+            try
+            {
+                responseBodyMap["status"] = "OK";
+                using (var fs = filesData.Files[0].OpenReadStream())
+                {
+                    var parser = new Models.LogParser(fs);
+                    responseBodyMap["data"] = JsonSerializer.Serialize(new Models.LogEntryGroup("1.1.1.1", 12));
+                    return new JsonResult(responseBodyMap);
+                }
+            }
+            catch(Exception exc) {
+                this.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                responseBodyMap["status"] = "Server error";
+                responseBodyMap["message"] = exc.Message;
+                return new JsonResult(responseBodyMap);
+            }
+        }
+    }
+}
---
+++ Models/LogParser.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TrimanAssessment.Models
+{
+    public class LogParser
+    {
+        private LogParser() { } // ensure no parameterless instance creation
+        public LogParser(Stream logStream)
+        {
+            logFileStream = logStream;
+            var initialPosition = logFileStream.Position;
+            logFileStream.Position = 0;
+            if((this.logFileEncoding = detectEncoding(logStream)) == null)
+                throw new Exception("Unable to detect encoding for this file.");
+            StreamReader reader = null;
+            try
+            {
+                reader = new StreamReader(logFileStream);
+                logFileText = reader.ReadToEnd();
+            }
+            catch (IOException exc) {
+                throw new Exception(String.Format("{0}: read error on file.", exc.GetType().Name));
+            }
+            catch (OutOfMemoryException exc) {
+                throw new Exception(String.Format("{0}: out of memory.", exc.Message));
+            }
+            catch (Exception exc)
+            {
+                throw (exc);
+            }
+            var pattern = @"^#Software: .*?$\n#Version: .*?$\n#Date: .*?$\n#Fields: (?<fields>[\w*\-*\(*\)* ]*)";
+            Regex re = new Regex(pattern,
+                RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            var matches = re.Matches(logFileText,0);
+            var clientIPIndex = Array.FindIndex<String>(
+                matches[0].Groups[1].Value.Split(' '), 
+                s => s == "c-ip");
+            // end
+            logFileStream.Position = initialPosition;
+        }
+
+        protected bool ParseBlock()
+        {
+            return true;
+        }
+
+        protected bool ParseStreaming()
+        {
+            return true;
+        }
+
+        protected Encoding detectEncoding(Stream logStream)
+        {
+            var initialPosition = logStream.Position;
+            Encoding enc = null;
+            try
+            {
+                Ude.CharsetDetector cdet = new Ude.CharsetDetector();
+                cdet.Feed(logStream);
+                cdet.DataEnd();
+                if (cdet.Charset == null)
+                {
+                    throw (new Exception("Unknown encoding."));
+                }
+                enc = Encoding.GetEncoding(cdet.Charset);
+            }
+            catch (Exception exc)
+            {
+                enc = null;
+            }
+            finally
+            {
+                logStream.Position = initialPosition;
+            }
+            return enc;
+        }
+
+        private Stream logFileStream = null;
+        private Encoding logFileEncoding = null;
+        private String logFileText = null;
+    }
+}
